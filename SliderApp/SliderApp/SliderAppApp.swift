5fbe28b3b12d58ec73237e8e10c6f210ce0998e1
Ossamoon <osamu950623@gmail.com>
2020-10-29 17:03:43 +0900

    書籍に従いAppを完成させた

---
+++ SliderApp/SliderApp/SliderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderAppApp.swift
+//  SliderApp
+//
+//  Created by 齋藤修 on 2020/10/29.
+//
+
+import SwiftUI
+
+@main
+struct SliderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
